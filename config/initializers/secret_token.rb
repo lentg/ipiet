0c308f8a18ac69b49594f39ea71af6f1c01389d4
lentg <lentg@qq.com>
2012-12-28 23:46:56 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iipet::Application.config.secret_token = 'e10784d34238dc63fc7518e1da9f9a4f3bcface4975985cb56e3b739ae33e1d83c00fe8fda16bae5c9dca7e1c71904204176317fd1ba0b6c19befeb97967fdf0'
